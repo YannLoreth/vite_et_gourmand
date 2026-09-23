16e980f97ec761f9285f329d25962065eed90128
yannloreth <yann.loreth@gmail.com>
2026-09-23 19:42:15 +0200

    Mise en place du fichier Database.php

---
+++ src/config/database.php
@@ -0,0 +1,25 @@
+<?php
+
+require_once __DIR__ . '/../../vendor/autoload.php';
+
+use Dotenv\Dotenv;
+
+$dotenv = Dotenv::createImmutable(__DIR__ . '/../../');
+$dotenv->load();
+
+$host = $_ENV['DB_HOST'];
+$port = $_ENV['DB_PORT'];
+$database = $_ENV['DB_DATABASE'];
+$username = $_ENV['DB_USERNAME'];
+$password = $_ENV['DB_PASSWORD'];
+
+try {
+    $pdo = new PDO(
+        "mysql:host=$host;port=$port;dbname=$database;charset=utf8mb4",
+        $username,
+        $password,
+        [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]
+    );
+} catch (PDOException $e) {
+    die("Erreur de connexion à la base de données : " . $e->getMessage());
+}
