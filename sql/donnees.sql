99e73dfde28c33c6a2a2bc5c3d6a59db837c0c19
yannloreth <yann.loreth@gmail.com>
2026-09-24 09:48:04 +0200

    Ajout du jeu de données de test pour jours et horaires

---
+++ sql/donnees.sql
@@ -0,0 +1,17 @@
+INSERT INTO jours (libelle) VALUES
+('Lundi'),
+('Mardi'),
+('Mercredi'),
+('Jeudi'),
+('Vendredi'),
+('Samedi'),
+('Dimanche');
+
+INSERT INTO horaire (jours_id, horaire_ouverture, horaire_fermeture) VALUES
+(1, '09:00', '18:00'),
+(2, '09:00', '18:00'),
+(3, '09:00', '18:00'),
+(4, '09:00', '18:00'),
+(5, '09:00', '19:00'),
+(6, '10:00', '17:00'),
+(7, '10:00', '15:00');
